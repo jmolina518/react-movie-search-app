c8ed573bcdd141be01329b290c0d2c121cefdf0c
jmolina518 <jmolina518@gmail.com>
2020-12-29 08:04:25 -0600

    add .env file

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 function SearchMovies() {
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    console.log('submitting');
+
+    const query = 'Jurassic Park';
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+  };
   return (
-    <form className='form'>
+    <form className='form' onSubmit={searchMovies}>
       <label htmlFor='query' className='label'>
         Movie Name
       </label>
